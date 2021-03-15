2697480b648260d2e22fb1a5d5aee12d5300331b
Byung Moon Kim <kimbm2014@gmail.com>
2021-03-15 01:58:06 -0700

    signing up and logging in

--- pastagram/AppDelegate.swift
+++ pastagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "i6n8xSu1IQRZDtjgsfuyDHx4FdlzIYAa71T0r33G" // <- UPDATE
+                $0.clientKey = "0xAsprkiVeAVr0cEdkvgIg3yTmuNKG7xhC0MHEYg" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
